d78e96a5922cbdf19f3c86b44c33ebc886eab0a6
emmanuelramses <emmanuel.ramses@gmail.com>
2016-09-03 14:41:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.lastfmKey = 'last_fm_key_here';
-config.mashapeKey = 'mashape_key_here';
+config.lastfmKey = '9be0f0083a9117e4951dc128c77c5d65';
+config.mashapeKey = 'F1Iy1mCDormshIejxNv1msIeCeX5p1LWx24jsn64PklG2oJsXr';
 
 module.exports = config;
